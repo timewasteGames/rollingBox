f72ed16a99fdfea8f76a00cc80b531c41acea427
timewastegames <timewastegames@gmail.com>
2018-06-11 19:08:17 +0100

    Generated project.

---
+++ android/src/com/brussell/rollingbox/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brussell.rollingbox;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brussell.rollingbox.RollingBox;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RollingBox(), config);
+	}
+}
